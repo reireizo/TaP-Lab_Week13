546d5a6039a796de1728dea67587718d403eb058
Rei Reizo <nathan.irwin2021@gmail.com>
2024-11-18 14:08:58 -0500

    Weather Manager added
    Reads in API as JSON.

---
+++ Assets/WeatherManager.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class WeatherManager : MonoBehaviour
+{
+    private string jsonApi;
+    public string cityName = "Orlando";
+
+    public void Start()
+    {
+        jsonApi = "http://api.openweathermap.org/data/2.5/weather?q=" + cityName + ",us&mode=json&appid=550a6618486c8002a8c4f81bf99ca84e";
+        StartCoroutine(GetWeatherXML(OnXMLDataLoaded));
+    }
+
+    private IEnumerator CallAPI(string url, Action<string> callback)
+    {
+        using (UnityWebRequest request = UnityWebRequest.Get(url))
+        {
+            yield return request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.ConnectionError)
+            {
+                Debug.LogError($"network problem: {request.error}");
+            }
+            else if (request.result == UnityWebRequest.Result.ProtocolError)
+            {
+                Debug.LogError($"response error: {request.responseCode}");
+            }
+            else
+            {
+                callback(request.downloadHandler.text);
+            }
+        }
+    }
+
+    public IEnumerator GetWeatherXML(Action<string> callback)
+    {
+        return CallAPI(jsonApi, callback);
+    }
+
+    public void OnXMLDataLoaded(string data)
+    {
+        Debug.Log(data);
+    }
+}
